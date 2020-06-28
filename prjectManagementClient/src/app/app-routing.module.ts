882e40eaf40ecac150ccb91fe6ec7065922bfc0e
omar.bouchaala <Proxym_OBouchaala@dha.gov.ae>
2020-06-28 19:06:25 +0100

    add prject management client side

---
+++ prjectManagementClient/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import {UserComponent} from "./user/user.component";
+import {LoginComponent} from "./modules/general/login/login.component";
+
+
+const routes: Routes = [
+  { path: 'user', component: UserComponent },
+  { path: '', component: LoginComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
